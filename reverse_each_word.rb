46496f4856d4e7361d0a1fef479a5032bf12fd12
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-06 16:32:03 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   sentence = sentence.split("")
   
   sentence.each do |word|
-    reversed.reverse!(word)
+    reversed = word.reverse
   end 
   return reversed.join("")
   
